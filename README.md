ea14df618743ad70c3d7a30e3f4c5cfdfc9835f5
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2020-08-30 17:44:11 -0300

    README update dependencies section and added utils section

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
-# GOWATCH
+# Gowatch
+
+
 
 ## About
 
-This project is an API like YouTube for Zup bootcamp, its build in Spring.
+This project is an API alike YouTube API for Zup bootcamp, its build in Spring.
 
 Dependencies
 
+- Java JDK 11
 - Spring Boot DevTools
 - Spring Web
 - Lombok
 - Spring Data JPA
-- Spring Security
-- OAuth2 Resource Server
+- Junit
+- Spring Model Mapper
 
+
+
+### Utils
+
+ [Download gowatch insomnia project](https://drive.google.com/file/d/13MA3fv7AmL5S72YjpRhslsP4YeFUylT3/view?usp=sharing)
--- src/main/java/com/lucas/gowatch/controller/VideoController.java
+++ src/main/java/com/lucas/gowatch/controller/VideoController.java
@@ -20,7 +20,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
 import java.io.*;
 import java.util.List;
 
@@ -74,7 +73,7 @@ public class VideoController {
         // Manage file
         String fileName = storageService.store(videoFile);
         video.setVideoFile(fileName);
-        VideoResponse videoResponse = null;
+        VideoResponse videoResponse;
 
         // Response request
         try {
